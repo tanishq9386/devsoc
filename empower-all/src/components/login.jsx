6d74cee3c4d4f9e2a4d374f663de082dfabfe050
Tanishq Tiwari <tanishq9386@gmail.com>
2024-03-18 17:01:30 +0530

    Login done

--- empower-all/src/components/login.jsx
+++ empower-all/src/components/login.jsx
@@ -38,15 +38,16 @@ const Login = () => {
   return (
     <div className="login-container">
       <Navbar />
-      {/* <div className="login-div">
-        <h1>Log In</h1>
-        <p>
-          Don't have an account?{" "}
-          <a href="signup" className="signupLogin">
-            Register Now
-          </a>
-        </p>
-        <div className="input-label">Username</div>
+      <div className="mainLoginContainer">
+        <div className="loginDiv">
+          <h1>Login</h1>
+          <p>Log In using social networks<br/>
+          </p>
+          <div className="social-icons">
+          <a target="_blank" href="https://www.google.com"><img src={google} className="google"/></a><a target="_blank" href="https://www.facebook.com"><img src={facebook}/></a>
+          </div>
+          <img src={or} className="or"/>
+          <div className="input-label">Username</div>
         <input
           type="text"
           className="username"
@@ -64,27 +65,16 @@ const Login = () => {
           onBlur={validateForm}
         />
         {errors.password && <span className="error">{errors.password}</span>}
-        <label className="remember">
-          <input type="checkbox" />
-          <span className="rememberLabel">Remember Me</span>
-        </label>
-        <input
-          type="button"
-          value="Log In"
-          className="loginB"
-          onClick={handleSubmit}
-        />
-      </div>
-      <img src={loginImg} className="loginImg" /> */}
-      <div className="mainLoginContainer">
-        <div className="loginDiv">
-          <h1>Login</h1>
-          <p>Log In using social networks<br/>
+        <button type='button' className="loginbutton">
+          Login 
+        </button>
+        </div>
+        <div className="signUpLogin">
+          <h1>New Here?</h1>
+          <p>
+          Sign Up and discover a great amount of new opportunties
           </p>
-          <div className="social-icons">
-          <a target="_blank" href="https://www.google.com"><img src={google} className="google"/></a><a target="_blank" href="https://www.facebook.com"><img src={facebook}/></a>
-          </div>
-          <img src={or} className="or"/>
+          <button type="button" className="signUpLoginButton">Sign Up</button>
         </div>
       </div>
       <Footer />
